b1b9dfc351cd09dfe42f73ff99511224e4e498f7
Ryan <rharr080@uottawa.ca>
2024-02-24 18:58:35 -0500

    js function

--- public/wordle.js
+++ public/wordle.js
@@ -1,6 +1,22 @@
-var heigt= 6; // nombre d;essaie
+var height= 6; // nombre d;essaie
 var width= 5; // longueur du mots a devine
 var row=0; // essaie #
 var col=0; //lettre
 var gameOver = false;
-var word = "ALLOO";
+var word = "SQUID";
+
+window.onload = function(){
+    intialize();
+}
+
+function intialize(){
+    for (let r = 0; r < height; r++){
+        for (let c = 0; c < width; c++){
+            let tile = document.createElement("span");
+            tile.id = r.toString() + "-" + c.toString();
+            tile.classList.add("tile");
+            tile.innerText = "P";
+            document.getElementById("board").appendChild(tile);
+        }
+    }
+}
