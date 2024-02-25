2402a22870042cdfcc7785f5436d22a71b59136d
Ryan <rharr080@uottawa.ca>
2024-02-24 20:26:34 -0500

    fixed some issues

--- public/wordle.js
+++ public/wordle.js
@@ -9,9 +9,10 @@ window.onload = function(){
     intialize();
 }
 
-function intialize(){
-    for (let r = 0; r < height; r++){
-        for (let c = 0; c < width; c++){
+function intialize() {
+    for (let r = 0; r < height; r++) {
+        for (let c = 0; c < width; c++) {
+            // <span id="0-0" class="tile">P</span>
             let tile = document.createElement("span");
             tile.id = r.toString() + "-" + c.toString();
             tile.classList.add("tile");
@@ -20,51 +21,52 @@ function intialize(){
         }
     }
     document.addEventListener("keyup", (e) => {
-        if (gameOver) return;
-        if ("KeyA" <= e.code && e.code <= "KeyZ"){
-            if (col < width){
-                let currTile =document.getElementById(row.toString() + '-' + col.toString());
-                if (currTile.innerText == ""){
-                    currTile.innerText = e.code[3]
+        if (gameOver) return; 
+        if ("KeyA" <= e.code && e.code <= "KeyZ") {
+            if (col < width) {
+                let currTile = document.getElementById(row.toString() + '-' + col.toString());
+                if (currTile.innerText == "") {
+                    currTile.innerText = e.code[3];
                     col += 1;
                 }
             }
         }
-        else if (e.code == "Backspace"){
-            if (0 < col && col <= width){
+        else if (e.code == "Backspace") {
+            if (0 < col && col <= width) {
                 col -=1;
             }
-            let currTile =document.getElementById(row.toString() + '-' + col.toString());
+            let currTile = document.getElementById(row.toString() + '-' + col.toString());
             currTile.innerText = "";
         }
-        else if (e.code == "Enter"){
+        else if (e.code == "Enter") {
             update();
-            row += 1;
+            row += 1; 
             col = 0;
         }
-        if (!gameOver && row == height){
+        if (!gameOver && row == height) {
             gameOver = true;
             document.getElementById("answer").innerText = word;
         }
-    });
+
+    })
 }
 
-function update(){
+function update() {
     let correct = 0;
-    for (let c = 0; c < width; c++){
+    for (let c = 0; c < width; c++) {
         let currTile = document.getElementById(row.toString() + '-' + c.toString());
         let letter = currTile.innerText;
-        if (word[c] == letter){
+        if (word[c] == letter) {
             currTile.classList.add("correct");
             correct += 1;
-        }
-        else if (word.includes(letter)){
+        } 
+        else if (word.includes(letter)) {
             currTile.classList.add("present");
-        }
-        else{
+        } 
+        else {
             currTile.classList.add("absent");
         }
-        if (correct = width){
+        if (correct == width) {
             gameOver = true;
         }
     }
