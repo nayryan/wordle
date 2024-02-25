c436596f74e4c68212d6b0f045252d1307a09f01
Ryan <rharr080@uottawa.ca>
2024-02-24 20:01:01 -0500

    game logic, yellow, green, grey...

--- public/wordle.js
+++ public/wordle.js
@@ -29,7 +29,6 @@ function intialize(){
                     col += 1;
                 }
             }
-
         }
         else if (e.code == "Backspace"){
             if (0 < col && col <= width){
@@ -38,5 +37,35 @@ function intialize(){
             let currTile =document.getElementById(row.toString() + '-' + col.toString());
             currTile.innerText = "";
         }
+        else if (e.code == "Enter"){
+            update();
+            row += 1;
+            col = 0;
+        }
+        if (!gameOver && row == height){
+            gameOver = true;
+            document.getElementById("answer").innerText = word;
+        }
     });
+}
+
+function update(){
+    let correct = 0;
+    for (let c = 0; c < width; c++){
+        let currTile = document.getElementById(row.toString() + '-' + c.toString());
+        let letter = currTile.innerText;
+        if (word[c] == letter){
+            currTile.classList.add("correct");
+            correct += 1;
+        }
+        else if (word.includes(letter)){
+            currTile.classList.add("present");
+        }
+        else{
+            currTile.classList.add("absent");
+        }
+        if (correct = width){
+            gameOver = true;
+        }
+    }
 }
